764b527bbe8aad86b2b3b68499c7e14fb42b438e
OpenTritium <tritium404@hotmail.com>
2025-01-27 16:16:06 +0800

    Restrict route table functions to Linux systems and add compile-time error for unsupported OS

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,15 @@ mod utils;
 pub use ipv4::Ipv4RouteFlags;
 pub use ipv6::Ipv6RouteFlags;
 
+#[cfg(target_os = "linux")]
 pub fn get_ipv4_route_table() -> ipv4::Ipv4RouteTable {
     ipv4::Ipv4RouteTable::default()
 }
 
+#[cfg(target_os = "linux")]
 pub fn get_ipv6_route_table() -> ipv6::Ipv6RouteTable {
     ipv6::Ipv6RouteTable::default()
 }
 
-
+#[cfg(not(target_os = "linux"))]
+compile_error!("This crate can only be compiled on Linux systems.");
